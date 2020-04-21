9af0a2ef063ce42f4195f9820aceface78fde46b
dnovik <dnovik01@gmail.com>
2020-04-21 22:40:42 +0300

    fixed

--- HW_2/HW_2.sql
+++ HW_2/HW_2.sql
@@ -17,12 +17,11 @@ where UnitPrice = (select min(UnitPrice) from Warehouse.StockItems);
 --представьте 3 способа (в том числе с CTE)
 
 with Top5Customers_CTE
-as
-(select top 5
-c.CustomerName, 
-c.CreditLimit, 
-c.IsStatementSent, 
-ct.TransactionAmount from Sales.CustomerTransactions ct
+as (select top 5
+	c.CustomerName, 
+	c.CreditLimit, 
+	c.IsStatementSent, 
+	ct.TransactionAmount from Sales.CustomerTransactions ct
 left join Sales.Customers c
 on c.CustomerID=ct.CustomerID
 order by TransactionAmount desc)
@@ -34,10 +33,10 @@ select top 5
 	CreditLimit,
 	IsStatementSent
 from Sales.Customers
-where CustomerID in (select top 5 CustomerID
+where CustomerID in 
+	(select top 5 CustomerID
 	from Sales.CustomerTransactions 
 	order by TransactionAmount desc);
-
 
 
 select top 5 CustomerID
@@ -54,13 +53,13 @@ inner join Application.Cities as cs
 on cs.CityID=c.PostalCityID
 inner join Application.People as p
 on p.PersonID=o.SalespersonPersonID
-where OrderID in (
-select OrderID from Sales.OrderLines
-where StockItemID in
-(select top 3 with ties
-StockItemID
-from Warehouse.StockItems
-order by UnitPrice desc))
+	where OrderID in (
+	select OrderID from Sales.OrderLines
+	where StockItemID in
+		(select top 3 with ties
+		StockItemID
+		from Warehouse.StockItems
+		order by UnitPrice desc))
 
 
 --5. ќбъ€сните, что делает и оптимизируйте запрос:
@@ -68,23 +67,20 @@ SELECT
 Invoices.InvoiceID,
 Invoices.InvoiceDate,
 (SELECT People.FullName
-FROM Application.People
-WHERE People.PersonID = Invoices.SalespersonPersonID
-) AS SalesPersonName,
+	FROM Application.People
+	WHERE People.PersonID = Invoices.SalespersonPersonID) AS SalesPersonName,
 SalesTotals.TotalSumm AS TotalSummByInvoice,
 (SELECT SUM(OrderLines.PickedQuantity*OrderLines.UnitPrice)
-FROM Sales.OrderLines
-WHERE OrderLines.OrderId = (SELECT Orders.OrderId
-FROM Sales.Orders
-WHERE Orders.PickingCompletedWhen IS NOT NULL
-AND Orders.OrderId = Invoices.OrderId)
-) AS TotalSummForPickedItems
+	FROM Sales.OrderLines
+	WHERE OrderLines.OrderId = (SELECT Orders.OrderId
+	FROM Sales.Orders
+	WHERE Orders.PickingCompletedWhen IS NOT NULL
+	AND Orders.OrderId = Invoices.OrderId)) AS TotalSummForPickedItems
 FROM Sales.Invoices
 JOIN
 (SELECT InvoiceId, SUM(Quantity*UnitPrice) AS TotalSumm
-FROM Sales.InvoiceLines
-GROUP BY InvoiceId
-HAVING SUM(Quantity*UnitPrice) > 27000) AS SalesTotals
-
+	FROM Sales.InvoiceLines
+	GROUP BY InvoiceId
+	HAVING SUM(Quantity*UnitPrice) > 27000) AS SalesTotals
 ON Invoices.InvoiceID = SalesTotals.InvoiceID
 ORDER BY TotalSumm DESC;
